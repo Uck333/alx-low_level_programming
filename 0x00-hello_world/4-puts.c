90532430ba8eadd5534d5b39a6278821918661ba
Uck@505 <uzohchukwuikem@yahoo.com>
2022-03-12 00:26:30 +0000

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+/* C program to print "Programming is like building a multilingual puzzle */
+#include <studio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
