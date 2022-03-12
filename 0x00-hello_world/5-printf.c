895e9d8f06fe7c47a15d7f817a69cd834a1aa2b9
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-12 09:28:30 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,15 @@
-/* C Program to use printf */
-#include <studio.h>
+/**
+ * main - nothing to declare yet
+ * @void - nil
+ *
+ * Description: displays on standard output using printf
+ * Return: zero if successful
+ */
+
+#include <stdio.h>
+
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
