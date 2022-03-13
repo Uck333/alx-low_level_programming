1efb8ac985e7186acc7c97850b888bcb62c8dfd4
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-13 01:17:58 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,9 @@
  * Description: c program to use fprintf
  * Return: one if successful
  */
-
 #include <stdio.h>
-
 int main(void)
 {
-        fprintf( stderr, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n");
-
-        return (1);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
