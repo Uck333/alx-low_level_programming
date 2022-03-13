578f403ddd33b3740854a2691a24490918910345
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-13 01:05:52 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/**
+ * main - nothing to declare
+ * @void - no argument
+ *
+ * Description: c program to use fprintf
+ * Return: one if successful
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+        fprintf( stderr, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n");
+
+        return (1);
+}
