f7d84252f97adc4dd428a7f4a04a75888316c9c1
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-12 23:53:16 -0800

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,17 +11,17 @@
 int main(void)
 
 {
-	 char c;
-        int I;
-        long int l;
-        long long int m;
-        float f;
+	char c;
+	int i;
+	long int l;
+	long long int m;
+	float f;
 
 	printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
-        printf("Size of an int: %lu.\n", (unsigned long)sizeof(I));
-        printf("Size of a long int: %lu.\n",(unsigned long)sizeof(l));
-        printf("Size of a long long int: %lu.\n",(unsigned long)sizeof(m));
-        printf("Size of a float: %lu.\n",(unsigned long)sizeof(f));
+	printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu.\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(m));
+	printf("size of a float: %lu.\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
