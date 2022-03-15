6a52a087c1e2d725086c6f93aef2100d66a9430f
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-14 22:00:54 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+/**
+ * main - nil to declare
+ * @void - nil
+ *
+ * Description: c program for alpha
+ * Return: nil if successful
+ */
+#include <stdio.h>
+int main(void)
+{
+	char i;
+	for(i='a'; i<='z'; i++);
+	putchar(i);
+		return (0);
+}
