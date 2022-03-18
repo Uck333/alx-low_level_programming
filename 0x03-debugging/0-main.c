a49bf742b783c5ca90046c2cdf519ff7481c1054
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-18 14:07:11 -0700

    debugging_0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+#include <stdio.h>
+int main(void)
+{
+        int i;
+
+        i = 98;
+        void positive_or_negative(i);
+
+        return (0);
+}
