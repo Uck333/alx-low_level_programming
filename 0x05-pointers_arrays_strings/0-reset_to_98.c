38b6acbe2129d6f3392293a2d418194b8b3d6b28
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-22 05:27:24 -0700

    task o

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,6 +11,13 @@
 void reset_to_98(int *n) {
 	*n = 98;
 }
-int main (void) {
-	return (0);
+int main(void)
+{
+    int n;
+
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
 }
