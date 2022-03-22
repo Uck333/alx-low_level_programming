e85beeecbf18e0e3a9c004f5f4504667c468066c
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-21 20:58:34 -0700

    updating the value the pointer to an int points to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks  the code
+ * @void - a useless integer
+ *
+ * reset_to_98 - a function to update a value
+ * Return: always zero
+ */
+void reset_to_98(int *n)
+	n = 98;
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Directory for practicing pointers and arrays
