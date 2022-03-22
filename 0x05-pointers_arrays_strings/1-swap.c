b9dc5ddd2849737a1bfbaace17a293853ed6dc40
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-22 15:14:34 -0700

    sec task

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,25 +1,14 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - checks the code
- * @var - nil to declare
+ * swap_int - checks the code
+ * @a: nil to declare
+ * @b: nil too
  *
- * swap_int: to swap two integers
  * Return: always nil
  */
-void swap_int(int *a, int *b) {
+void swap_int(int *a, int *b)
+{
 	*a = 42;
 	*b = 98;
-}
-int main(void)
-{
-    int a;
-    int b;
-
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
 }
