ce4393581ec70e1449f7b9495adf8bf6d35176b6
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-22 11:46:58 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - checks the code
+ * @var - nil
+ *
+ * print: prints a string in reverse
+ * Return: zero
+ */
+void print_rev(char *s) {
+	char *s = "I do not fear computers. I fear the lack of them - Isaac Asimov"
+}
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    print_rev(str);
+    return (0);
+}
