1a8b1f7a4e3c2881d5c362985d7023084455dec4
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-22 12:05:48 -0700

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - checks the code
+ * @var - nil
+ *
+ * rev_string: reverserls a string
+ * Return: zero
+ */
+void rev_string(char *s) {
+	char *s = "My School"
+}
+int main(void)
+{
+    char s[10] = "My School";
+
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
+}
