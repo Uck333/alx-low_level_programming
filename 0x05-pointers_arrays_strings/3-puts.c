77dd94a2d843a63cd77166460275a01dd8acdb99
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-22 11:34:40 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - checks the code
+ * @var - nil
+ *
+ * puts: to print a string
+ * Return: zero
+ */
+void _puts(char *str) {
+	*str = "I do not fear computers. I fear the lack of them - Isaac Asimov"}
+	int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
+}
