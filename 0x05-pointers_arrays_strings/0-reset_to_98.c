fc6a52d43e7a06e57e44e3f84097327e6ecefa2a
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-22 14:52:11 -0700

    task o

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - checks  the code
- * @void - a useless integer
- *
  * reset_to_98 - a function to update a value
+ * n - parameter value
+ *
  * Return: always zero
  */
-void reset_to_98(int *n) {
+void reset_to_98(int *n) 
+{
 	*n = 98;
 }
