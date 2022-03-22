5a247f5c9acf43b9165f9680be36dac093607691
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-22 11:19:51 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - to check the code
+ * @var - nil to declare
+ *
+ * strlen: returns the length of a string
+ * Return: always nil
+ */
+int _strlen(char *s) {
+	char *s = str;
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@
 void reset_to_98(int *n);
 void puts_half(char *str);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
