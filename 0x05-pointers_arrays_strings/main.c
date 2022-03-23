efd6b3726cf0375bdc9cdcf4d1cdd3836e8c8539
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-22 21:52:35 -0700

    puts

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+#include " main.h"
 int main(void)
 {
     char *str;
-    int len;
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
     return (0);
-}
+
