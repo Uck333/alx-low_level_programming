e84ea2b1fb6482ec0f093742c28f27193ea923e5
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-22 20:30:55 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - to check the code
- * @var - nil to declare
+ * _strlen - check the code
+ * @s: -nil to declare
  *
- * strlen: returns the length of a string
  * Return: always nil
  */
-int _strlen(char *s) {
+int _strlen(char *s)
+{
 	char *s = str;
 }
 
