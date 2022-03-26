4dca6a183cc81489c45eb42be7ddd4ba2b67a0ec
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-25 19:11:29 -0700

    lookup

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strncat - to concatenate two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: always nil
+ */
+char *string_toupper(char *)
+{
+	char *[] = " Look up!\n";
+	char *ptr;
+}
