a6c19014931d4449791a18cec7d12bde7cf771c4
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-25 18:26:35 -0700

    rev

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reverse_array - to concatenate two strings
+ * @a: estimation string
+ * @n: number string
+ *
+ * Return: always nil
+ */
+void reverse_array(int *a, int n)
+{
+	*a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+}
