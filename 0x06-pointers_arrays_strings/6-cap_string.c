a9de04338724ead24710656fde4de5d3ccc601e0
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-25 19:40:26 -0700

    cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * cap_string - to concatenate two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: always nil
+ */
+char *cap_string (char *)
+{
+	*[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char *ptr;
