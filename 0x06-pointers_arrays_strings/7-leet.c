b290a0d3398e508b91eab1840233c562d7b12185
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-25 19:53:04 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcat - to concatenate two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: always nil
+ */
+char *leet(char *)
+{
+	*[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+	char *p;
+}
