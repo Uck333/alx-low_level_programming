0eccc6aa81b14c5ea3fc7afb32a1f6bb6bac970e
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-25 17:04:22 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strncat - to concatenate two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: always nil
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	dest[] = "Hello ";
+	src[] ="World!\n" ;
+	char *ptr;
+}
