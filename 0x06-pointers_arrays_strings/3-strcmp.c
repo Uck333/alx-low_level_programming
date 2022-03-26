e8c30a26acf211e49114a12ebc1acced66186f05
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-25 17:42:29 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcmp - to concatenate two strings
+ * @s1: estination string
+ * @s2: source string
+ *
+ * Return: always nil
+ */
+int _strcmp(char *s1, char *s2)
+{
+	*s1[] = "Hello";
+	*s2[] = "World!";
+}
