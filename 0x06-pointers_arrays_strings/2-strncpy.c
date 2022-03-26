91464a9bdacbae41634485df0bb2954e521862a8
Uzoh <99915841+Uck505@users.noreply.github.com>
2022-03-25 17:23:24 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcat - to concatenate two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: always nil
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	*dest[] = "hello ";
+	*src[] = "world!\n";
+	char *str;
