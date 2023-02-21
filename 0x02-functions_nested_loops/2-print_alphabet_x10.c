10e6056b0f98f6fd54da2d50900644bb7b5811e7
Uck333 <uzohk737@gmail.com>
2023-02-21 12:24:12 -0800

    functions and nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,10 +12,13 @@
 void print_alphabet_x10(void)
 {
 	char i;
+	int j = '0';
 
 	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
+		if (j <= 9)
+		{
+			_putchar(i);
+			j++;
+		}
 	_putchar('\n');
 }
