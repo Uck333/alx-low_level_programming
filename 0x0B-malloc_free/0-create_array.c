8bbe4dac3123e4058f4d184617743d2bfc69881e
uck <uzohk737@gmail.com>
2023-09-11 19:45:47 -0700

    function-like macro

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * create_array - makes an array of chars...
+ * @size: An unsigned int variable
+ * @c: A char variable
+ *
+ * Description: creates an array of chars, and initializes it to a char
+ * Return: zero
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *b;
+	unsigned int i;
+	char *p;
+
+	b = (char *)malloc(size * sizeof(char));
+
+	if ((b == NULL) || (size == 0))
+	{
+		return (NULL);
+	}
+	else
+		for (i = 0; i < size; i++)
+		{
+			p[0] = c[i];
+		}
+	free(b);
+	return (p);
+}
+
