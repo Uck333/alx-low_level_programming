a122de6bf664dffb04c3f4c8494586b6a12719b7
uck <uzohk737@gmail.com>
2023-09-12 18:15:24 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog
+#define dog
+
+/**
+ * struct dog - A struct to be defined
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Program defines struct with three member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
