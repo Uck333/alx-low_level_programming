200497d5bc08535535be9a336d8ea5e5559993b2
uck <uzohk737@gmail.com>
2023-11-11 02:58:47 +0000

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,27 +3,29 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - prints the name of the program
- * @n: represents the size/number of arguments passed to the fuction
+ * print_numbers - prints the name of the program
+ * @separator: represents the size/number of arguments passed to the fuction
+ * @n: number of arguments passed
  * @...: points to the array of strings constituting the arguements
  *
  * Description: A program that prints its own name
  * Return: always zero
  */
 
-int sum_them_all(const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list a;
+	va_list arg;
 	unsigned int i;
 	int sum = 0;
 
-	va_start(a, n);
+	va_start(arg, n);
 
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(a, int);
+		sum = va_arg(a, int);
+		printf("%c
 	}
 	va_end(a);
 	return (sum);
--- 0x10-variadic_functions/main.h
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-
